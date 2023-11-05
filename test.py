6ced6acfc3f11dcb66516c28bd7586b91e32940f
RandomUserSureWhyNot <149949325+RandomUserSureWhyNot@users.noreply.github.com>
2023-11-05 14:06:45 +0000

    Test File - Requires chromedriver

---
+++ test.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+driver = webdriver.Chrome()
+driver.maximize_window()
+
+wait = WebDriverWait(driver, 3)
+presence = EC.presence_of_element_located
+visible = EC.visibility_of_element_located
+
+driver.get('https://en.wikipedia.org/wiki/Main_Page')
+
+time.sleep(2)
+search = driver.find_element(By.ID, "searchInput")
+search.send_keys("sneed")
+time.sleep(1)
+search.send_keys(Keys.RETURN)
+
+time.sleep(2)
+findPage = driver.find_element(By.LINK_TEXT, "Chris Sneed")
+findPage.click()
+
+time.sleep(5)
+driver.get('https://www.youtube.com/results?search_query={}'.format(str("Jordan Peterson Gives a Lecture on SNEED")))
+
+time.sleep(4)
+
+wait.until(visible((By.ID, "video-title")))
+driver.find_element(By.ID, "video-title").click()
+
+while(True):
+    pass
